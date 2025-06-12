5aa18c2bf7a3e1e01747fccf250cc23c56ce7e3b
Bangeunseong <bangeunseong@naver.com>
2025-06-13 01:55:33 +0900

    [feat] Added InputSystem Asset(InputActions), Added Skeleton Code that Player entity needs

---
+++ Assets/01.Scripts/Entity/Player/Scripts/PlayerInteraction.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace _01.Scripts.Entity.Player.Scripts
+{
+    public class PlayerInteraction : MonoBehaviour
+    {
+        
+        
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
