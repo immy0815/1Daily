aae23d8791a215f19164b733c3bdba066112ef26
Zi-woo <jiou64@naver.com>
2025-06-13 19:53:22 +0900

    [refactor] Weapon Ithrowable추가
    Weapon Ithrowable추가

--- Assets/01.Scripts/Entity/Weapon/Weapon.cs
+++ Assets/01.Scripts/Entity/Weapon/Weapon.cs
@@ -1,10 +1,11 @@
 using UnityEngine;
 
-public abstract class Weapon : MonoBehaviour
+public abstract class Weapon : MonoBehaviour, IThrowable
 {
     public WeaponData weaponData;
      public virtual void Equip() { }
 
     public virtual void Unequip() { }
-    
+
+    public void OnThrow(Vector3 force) { }
 }
