2b8adfa5f37e0e771b80fce04c1cef016f3f6e3e
Bangeunseong <bangeunseong@naver.com>
2025-06-13 14:47:13 +0900

    [feat] Added Basic Player FSM Structure

---
+++ Assets/01.Scripts/Entity/Player/Scripts/States/Ground/PlayerRunState.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace _01.Scripts.Entity.Player.Scripts.States.Ground
+{
+    public class PlayerRunState : PlayerGroundState
+    {
+        public PlayerRunState(PlayerStateMachine machine) : base(machine)
+        {
+        }
+        
+        public override void Enter()
+        {
+            base.Enter();
+            StartAnimation(stateMachine.Player.AnimationData.RunParameterHash);
+        }
+
+        public override void Exit()
+        {
+            base.Exit();
+            StopAnimation(stateMachine.Player.AnimationData.RunParameterHash);
+        }
+        
+        public override void Update()
+        {
+            base.Update();
+
+            if (stateMachine.Player.CharacterController.velocity.magnitude < playerCondition.RunSpeedThreshold)
+                stateMachine.ChangeState(stateMachine.WalkState);
+        }
+    }
+}
