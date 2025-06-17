01f9c44087c253da73d1d0fcb4283f4cdbb17f87
TaeYoungTOF <113530625+TaeYoungTOF@users.noreply.github.com>
2025-06-18 08:46:50 +0900

    temp
    temp

--- Assets/01.Scripts/Manager/SceneLoader.cs
+++ Assets/01.Scripts/Manager/SceneLoader.cs
@@ -1,76 +1,76 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.AddressableAssets;
-using UnityEngine.ResourceManagement.AsyncOperations;
-using UnityEngine.ResourceManagement.ResourceProviders;
-using UnityEngine.SceneManagement;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.AddressableAssets;
+// using UnityEngine.ResourceManagement.AsyncOperations;
+// using UnityEngine.ResourceManagement.ResourceProviders;
+// using UnityEngine.SceneManagement;
 
-public class SceneLoader
-{
-    private Dictionary<SceneName, string> _sceneKeyMap;
-    private AsyncOperationHandle<SceneInstance>? _currentSceneHandle;
+// public class SceneLoader
+// {
+//     private Dictionary<SceneName, string> _sceneKeyMap;
+//     private AsyncOperationHandle<SceneInstance>? _currentSceneHandle;
 
-    public void Init()
-    {
-        _sceneKeyMap = new Dictionary<SceneName, string>
-        {
-            { SceneName.Game, "Scene_Game" }
-        };
-    }
+//     public void Init()
+//     {
+//         _sceneKeyMap = new Dictionary<SceneName, string>
+//         {
+//             { SceneName.Game, "Scene_Game" }
+//         };
+//     }
 
-    // ResourceManager에서만 접근하는 메서드입니다!! ResourceManager의 SwitchScene을 호출해주세요!
-    public IEnumerator LoadSceneAsync(SceneName name)
-    {
-        if (_currentSceneHandle.HasValue)
-        {
-            Addressables.Release(_currentSceneHandle.Value);
-            _currentSceneHandle = null;
-        }
+//     // ResourceManager에서만 접근하는 메서드입니다!! ResourceManager의 SwitchScene을 호출해주세요!
+//     public IEnumerator LoadSceneAsync(SceneName name)
+//     {
+//         if (_currentSceneHandle.HasValue)
+//         {
+//             Addressables.Release(_currentSceneHandle.Value);
+//             _currentSceneHandle = null;
+//         }
 
-        if (!_sceneKeyMap.TryGetValue(name, out var sceneKey))
-        {
-            Debug.LogError($"[SceneLoader] No scene key mapped for {name}");
-            yield break;
-        }
+//         if (!_sceneKeyMap.TryGetValue(name, out var sceneKey))
+//         {
+//             Debug.LogError($"[SceneLoader] No scene key mapped for {name}");
+//             yield break;
+//         }
 
-        var handle = Addressables.LoadSceneAsync(sceneKey, LoadSceneMode.Single);
-        yield return handle;
+//         var handle = Addressables.LoadSceneAsync(sceneKey, LoadSceneMode.Single);
+//         yield return handle;
 
-        if (handle.Status == AsyncOperationStatus.Succeeded)
-        {
-            _currentSceneHandle = handle;
-            Debug.Log($"[SceneLoader] Scene <{name}> loaded successfully.");
-        }
-        else
-        {
-            Debug.LogError($"[SceneLoader] Failed to load scene <{name}>.");
-        }
-    }
+//         if (handle.Status == AsyncOperationStatus.Succeeded)
+//         {
+//             _currentSceneHandle = handle;
+//             Debug.Log($"[SceneLoader] Scene <{name}> loaded successfully.");
+//         }
+//         else
+//         {
+//             Debug.LogError($"[SceneLoader] Failed to load scene <{name}>.");
+//         }
+//     }
 
-    // ResourceManager에서만 접근하는 메서드입니다!! ResourceManager의 SwitchScene을 호출해주세요!
-    public IEnumerator UnloadSceneAsync(SceneName name)
-    {
-        var scene = SceneManager.GetSceneByName(name.ToString());
+//     // ResourceManager에서만 접근하는 메서드입니다!! ResourceManager의 SwitchScene을 호출해주세요!
+//     public IEnumerator UnloadSceneAsync(SceneName name)
+//     {
+//         var scene = SceneManager.GetSceneByName(name.ToString());
 
-        if (scene.isLoaded)
-        {
-            var unloadOp = SceneManager.UnloadSceneAsync(scene);
-            yield return unloadOp;
+//         if (scene.isLoaded)
+//         {
+//             var unloadOp = SceneManager.UnloadSceneAsync(scene);
+//             yield return unloadOp;
 
-            if (unloadOp.isDone)
-            {
-                Debug.Log($"[SceneLoader] Scene <{name}> unloaded successfully.");
-            }
-            else
-            {
-                Debug.LogError($"[SceneLoader] Failed to unload scene <{name}>.");
-            }
-        }
-        else
-        {
-            Debug.LogWarning($"[SceneLoader] Scene <{name}> is not currently loaded.");
-            yield return null;
-        }
-    }
-}
+//             if (unloadOp.isDone)
+//             {
+//                 Debug.Log($"[SceneLoader] Scene <{name}> unloaded successfully.");
+//             }
+//             else
+//             {
+//                 Debug.LogError($"[SceneLoader] Failed to unload scene <{name}>.");
+//             }
+//         }
+//         else
+//         {
+//             Debug.LogWarning($"[SceneLoader] Scene <{name}> is not currently loaded.");
+//             yield return null;
+//         }
+//     }
+// }
