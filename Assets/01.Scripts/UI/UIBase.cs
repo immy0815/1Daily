4fc2f6b9dca92f5bb0a7d1a78fbd17561a27a3f5
ChoiYoungIm <keycys11@naver.com>
2025-06-17 21:02:55 +0900

    [feat] Text Effect 구현

---
+++ Assets/01.Scripts/UI/UIBase.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIBase : MonoBehaviour
+{
+    [SerializeField] protected CanvasGroup canvasGroup;
+    [SerializeField] private Canvas canvas;
+    
+    protected virtual void Reset()
+    {
+        canvasGroup = GetComponent<CanvasGroup>();
+        canvas = GetComponent<Canvas>();
+    }
+    
+    public virtual void Initialization()
+    {
+        canvasGroup.SetAlpha(0);
+
+        if (canvas != null)
+        {
+            canvas.worldCamera = UIManager.Instance.UICamera;
+            canvas.planeDistance = 1;
+        }
+    }
+    
+    public virtual void Open()
+    {
+        canvasGroup.SetAlpha(1);
+    }
+    
+    public virtual void Close()
+    {
+        canvasGroup.SetAlpha(0);
+    }
+}
