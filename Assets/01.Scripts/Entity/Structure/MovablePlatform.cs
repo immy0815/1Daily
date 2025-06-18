de74007c2bd91d13a5ffd64dba3f3210df15876c
AmteurPotion <1k0j2i4@gmail.com>
2025-06-18 19:22:21 +0900

    [Feat] 움직이는 발판 작업

--- Assets/01.Scripts/Entity/Structure/MovablePlatform.cs
+++ Assets/01.Scripts/Entity/Structure/MovablePlatform.cs
@@ -1,10 +1,11 @@
 using System.Collections.Generic;
-using _01.Scripts.Manager;
 using UnityEngine;
 
 public class MovablePlatform : MonoBehaviour
 {
-  public float speed = 10f;
+  public float speed = 1f;
+  public MoveType moveType = MoveType.Fixed;
+  public TimeScaleMode timeScaleMode = TimeScaleMode.Scaled;
   public List<Vector3> positionList = new();
   public int index = 0;
   
@@ -16,12 +17,31 @@ public class MovablePlatform : MonoBehaviour
       if (index >= positionList.Count) index = 0;
     }
     
-    transform.position = Vector3.MoveTowards(transform.position, positionList[index], speed * Time.unscaledDeltaTime);
+    var timeScale = timeScaleMode == TimeScaleMode.Scaled ? Time.deltaTime : Time.unscaledDeltaTime;
+
+    if (moveType == MoveType.Lerp)
+    {
+      transform.position = Vector3.Lerp(transform.position, positionList[index], speed * timeScale);
+    }
+    else if (moveType == MoveType.Fixed)
+    {
+      transform.position += (positionList[index] - transform.position).normalized * (speed * timeScale);
+    }
   }
   
-  [ContextMenu("Add Position")]
+  [ContextMenu("Add Current Position")]
   private void AddPosition()
   {
     positionList.Add(transform.position);
   }
+}
+
+public enum MoveType
+{
+  Lerp, Fixed
+}
+
+public enum TimeScaleMode
+{
+  Scaled, Unscaled
 }
